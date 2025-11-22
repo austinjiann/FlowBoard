d6a5eb09584d5e9a89df159d624743a58e785e34
JLi2007 <LiJames9532@gmail.com>
2025-11-22 00:37:13 -0500

    feat: cleaned up client 🥰

---
+++ frontend/src/pages/Landing.tsx
@@ -0,0 +1,9 @@
+function Landing() {
+    return (
+        <div style={{ position: 'fixed', inset: 0 }}>
+            hello this is the landing page
+        </div>
+    )
+}
+
+export default Landing;
