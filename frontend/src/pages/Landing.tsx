1452463bfab63e3bd5a573ad547870633b990362
Phoebe <phoebe@bot>
2026-03-19 21:39:41 +0000

    phoebe: TES-7 - change landing page colour to blue

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -8,7 +8,7 @@ import Footer from "../components/landing/Footer";
 function Landing() {
   return (
     <Theme>
-      <div className="min-h-screen relative bg-white">
+      <div className="min-h-screen relative bg-blue-500">
         {/* Grid Pattern */}
         <div className="absolute inset-0 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-size-[24px_24px]"></div>
 
